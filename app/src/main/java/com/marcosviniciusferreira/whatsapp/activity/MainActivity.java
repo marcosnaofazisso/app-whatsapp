7162d5e1b5fff72f345144b5197a07c5894acc40
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2022-11-30 20:21:15 -0300

    adjusting background color and toolbar menu

--- app/src/main/java/com/marcosviniciusferreira/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/whatsapp/activity/MainActivity.java
@@ -2,6 +2,9 @@ package com.marcosviniciusferreira.whatsapp.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.marcosviniciusferreira.whatsapp.R;
 
@@ -12,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("WhatsApp");
+        toolbar.setTitleTextColor(this.getResources().getColor(android.R.color.white));
+        setSupportActionBar(toolbar);
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
