c949d2152d2f00090b7aa62bae5bb3c0d6c91851
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2022-12-16 14:54:00 -0300

    allowing camera view to update to profile

--- app/src/main/java/com/marcosviniciusferreira/whatsapp/activity/ConfiguracoesActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/whatsapp/activity/ConfiguracoesActivity.java
@@ -7,13 +7,21 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.marcosviniciusferreira.whatsapp.R;
 import com.marcosviniciusferreira.whatsapp.helper.Permissao;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
+
+    private ImageButton imageButtonCamera, imageButtonGaleria;
+    private static final int SELECAO_CAMERA = 100;
+    private static final int SELECAO_GALERIA = 200;
 
     public String[] permissoesNecessarias = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -25,6 +33,9 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
 
+        imageButtonCamera = findViewById(R.id.imageButtonCamera);
+        imageButtonGaleria = findViewById(R.id.imageButtonGaleria);
+
 
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Configurações");
@@ -35,6 +46,25 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         Permissao.validarPermissoes(permissoesNecessarias, this, 1);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        imageButtonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (i.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(i, SELECAO_CAMERA);
+                }
+            }
+        });
+
+        imageButtonGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
