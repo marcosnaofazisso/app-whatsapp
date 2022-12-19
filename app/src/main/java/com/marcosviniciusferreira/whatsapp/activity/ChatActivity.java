456219a1a867ffbacb58917b6248dc74d24c009c
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2022-12-19 13:10:22 -0300

    add design of chat activity and content

--- app/src/main/java/com/marcosviniciusferreira/whatsapp/activity/ChatActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/whatsapp/activity/ChatActivity.java
@@ -19,16 +19,9 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
