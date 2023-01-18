2183c17a9d7d2cef8ce54733f764e0db67723343
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-01-18 08:33:05 -0300

    add selected contacts adapter and horizontal layout manager

--- app/src/main/java/com/marcosviniciusferreira/whatsapp/activity/GrupoActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/whatsapp/activity/GrupoActivity.java
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
+import android.widget.AdapterView;
 
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -19,7 +20,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 import com.marcosviniciusferreira.whatsapp.R;
 import com.marcosviniciusferreira.whatsapp.adapter.ContatosAdapter;
+import com.marcosviniciusferreira.whatsapp.adapter.GrupoSelecionadoAdapter;
 import com.marcosviniciusferreira.whatsapp.config.FirebaseConfig;
+import com.marcosviniciusferreira.whatsapp.helper.RecyclerItemClickListener;
 import com.marcosviniciusferreira.whatsapp.helper.UsuarioFirebase;
 import com.marcosviniciusferreira.whatsapp.model.Usuario;
 
@@ -30,7 +33,9 @@ public class GrupoActivity extends AppCompatActivity {
 
     private RecyclerView recyclerMembros, recyclerMembrosSelecionados;
     private ContatosAdapter contatosAdapter;
+    private GrupoSelecionadoAdapter grupoSelecionadoAdapter;
     private List<Usuario> listaMembros = new ArrayList<>();
+    private List<Usuario> listaMembrosSelecionados = new ArrayList<>();
     private ValueEventListener valueEventListenerMembros;
     private DatabaseReference usuariosRef;
     private FirebaseUser usuarioAtual;
@@ -59,18 +64,65 @@ public class GrupoActivity extends AppCompatActivity {
 
         recyclerMembros = findViewById(R.id.recyclerMembros);
         recyclerMembrosSelecionados = findViewById(R.id.recyclerMembrosSelecionados);
+
         usuariosRef = FirebaseConfig.getFirebaseDatabase().child("usuarios");
         usuarioAtual = UsuarioFirebase.getUsuarioAtual();
 
 
-        //Configurar adapter
+        //Configurar adapter para os Contatos
         contatosAdapter = new ContatosAdapter(listaMembros, getApplicationContext());
 
-        //Configurar recyclerview
+        //Configurar recyclerview para os Contatos
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerMembros.setLayoutManager(layoutManager);
         recyclerMembros.setHasFixedSize(true);
         recyclerMembros.setAdapter(contatosAdapter);
+
+        recyclerMembros.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getApplicationContext(),
+                        recyclerMembros,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+
+                                Usuario usuarioSelecionado = listaMembros.get(position);
+
+                                //Remover usuario selecionado da lista
+                                listaMembros.remove(usuarioSelecionado);
+                                contatosAdapter.notifyDataSetChanged();
+
+                                //Adicionar usuario selecionado na nova lista
+                                listaMembrosSelecionados.add(usuarioSelecionado);
+                                grupoSelecionadoAdapter.notifyDataSetChanged();
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            }
+                        }
+                )
+        );
+
+
+        //Configurar adapter para os Membros Selecionados
+        grupoSelecionadoAdapter = new GrupoSelecionadoAdapter(listaMembrosSelecionados, getApplicationContext());
+
+        //Configurar recyclerview para os Membros Selecionados
+        RecyclerView.LayoutManager layoutManagerHorizontal = new LinearLayoutManager(
+                getApplicationContext(),
+                LinearLayoutManager.HORIZONTAL,
+                false
+        );
+        recyclerMembrosSelecionados.setLayoutManager(layoutManagerHorizontal);
+        recyclerMembrosSelecionados.setHasFixedSize(true);
+        recyclerMembrosSelecionados.setAdapter(grupoSelecionadoAdapter);
 
 
     }
---
+++ app/src/main/java/com/marcosviniciusferreira/whatsapp/adapter/GrupoSelecionadoAdapter.java
@@ -0,0 +1,76 @@
+package com.marcosviniciusferreira.whatsapp.adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.marcosviniciusferreira.whatsapp.R;
+import com.marcosviniciusferreira.whatsapp.model.Usuario;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class GrupoSelecionadoAdapter extends RecyclerView.Adapter<GrupoSelecionadoAdapter.MyViewHolder> {
+
+    private List<Usuario> contatosSelecionados;
+    private Context context;
+
+    public GrupoSelecionadoAdapter(List<Usuario> listaContatos, Context c) {
+        this.contatosSelecionados = listaContatos;
+        this.context = c;
+    }
+
+    @NonNull
+    @Override
+    public GrupoSelecionadoAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemLista = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.adapter_grupo_selecionado, parent, false);
+        return new GrupoSelecionadoAdapter.MyViewHolder(itemLista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GrupoSelecionadoAdapter.MyViewHolder holder, int position) {
+
+        Usuario usuario = contatosSelecionados.get(position);
+
+        holder.nome.setText(usuario.getNome());
+
+        if (usuario.getFoto() != null) {
+            Uri uri = Uri.parse(usuario.getFoto());
+            Glide.with(context).load(uri).into(holder.foto);
+        } else {
+            holder.foto.setImageResource(R.drawable.padrao);
+
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contatosSelecionados.size();
+    }
+
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView foto;
+        TextView nome;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            foto = itemView.findViewById(R.id.imageViewFotoMembroSelecionado);
+            nome = itemView.findViewById(R.id.textNomeMembroSelecionado);
+
+        }
+    }
+}
