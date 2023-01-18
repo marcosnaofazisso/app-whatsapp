01bf0d7cb0e686b43d5ec47a43d2d3b7a66765dc
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-01-18 17:52:31 -0300

    listing selected members of group in confirmation acitivty

--- app/src/main/java/com/marcosviniciusferreira/whatsapp/activity/CadastroGrupoActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/whatsapp/activity/CadastroGrupoActivity.java
@@ -7,21 +7,35 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.marcosviniciusferreira.whatsapp.R;
+import com.marcosviniciusferreira.whatsapp.adapter.GrupoSelecionadoAdapter;
+import com.marcosviniciusferreira.whatsapp.helper.RecyclerItemClickListener;
 import com.marcosviniciusferreira.whatsapp.model.Usuario;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CadastroGrupoActivity extends AppCompatActivity {
 
     private List<Usuario> listaMembrosSelecionados = new ArrayList<>();
-    private TextView textTotalMembros;
+
+    private TextView textTotalParticipantes;
+    private RecyclerView recyclerParticipantes;
+    private CircleImageView imageGrupo;
+    private EditText editNomeGrupo;
+    private RecyclerView recyclerMembrosSelecionados;
+    private GrupoSelecionadoAdapter grupoSelecionadoAdapter;
 
 
     @Override
@@ -29,10 +43,15 @@ public class CadastroGrupoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_grupo);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Cadastrar Novo Grupo");
         setSupportActionBar(toolbar);
 
         //Configuracoes iniciais
-        textTotalMembros = findViewById(R.id.textTotal);
+        textTotalParticipantes = findViewById(R.id.textTotalParticipantes);
+        imageGrupo = findViewById(R.id.imageGrupo);
+        editNomeGrupo = findViewById(R.id.editNomeGrupo);
+        recyclerParticipantes = findViewById(R.id.recyclerParticipantes);
+
 
         FloatingActionButton fab = findViewById(R.id.fabAvancarCadastro);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -49,8 +68,18 @@ public class CadastroGrupoActivity extends AppCompatActivity {
             List<Usuario> membros = (List<Usuario>) getIntent().getExtras().getSerializable("membros");
             listaMembrosSelecionados.addAll(membros);
 
-            textTotalMembros.setText("Total: " + listaMembrosSelecionados.size());
+            textTotalParticipantes.setText("Participantes: " + listaMembrosSelecionados.size());
+
         }
+
+        //Configurar adapter para os Membros Selecionados
+        grupoSelecionadoAdapter = new GrupoSelecionadoAdapter(listaMembrosSelecionados, getApplicationContext());
+
+        //Configurar recyclerview para os Membros Selecionados
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerParticipantes.setLayoutManager(layoutManager);
+        recyclerParticipantes.setHasFixedSize(true);
+        recyclerParticipantes.setAdapter(grupoSelecionadoAdapter);
 
 
     }
--- app/src/main/java/com/marcosviniciusferreira/whatsapp/activity/GrupoActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/whatsapp/activity/GrupoActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
